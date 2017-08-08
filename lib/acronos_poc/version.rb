c6b5644437640e5372b3ccddf1727330eedf8d2c
Damien Le Thiec <damien.lethiec@gmail.com>
2017-08-07 20:45:06 -0400

    fix first mistake

--- lib/acronos_poc/version.rb
+++ lib/acronos_poc/version.rb
@@ -1,3 +1,3 @@
 module AcronosPoc
-  VERSION = "0.1.0"
+  VERSION = "0.1.1"
 end
--- lib/generators/acronos_poc/acronos_poc_generator.rb
+++ lib/generators/acronos_poc/acronos_poc_generator.rb
@@ -2,8 +2,8 @@ require 'rails/generators/base'
 
 class AcronosPocGenerator < Rails::Generators::Base
   source_root File.expand_path('../templates', __FILE__)
-  argument :controller, type: :string
-  argument :method, type: :string, default: "index"
+  argument :resource_name, type: :string
+  argument :method_name, type: :string, default: "index"
 
   def generate_view
     template "#{method}.html.erb", "app/views/#{controller}/#{method}.html.erb"
