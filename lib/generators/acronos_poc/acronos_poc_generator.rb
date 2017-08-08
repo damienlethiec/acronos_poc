6a7019c04e4ea454332d220b227653ce9eb08d6b
Damien Le Thiec <damien.lethiec@gmail.com>
2017-08-07 20:15:19 -0400

    add generator

--- lib/generators/acronos_poc/acronos_poc_generator.rb
+++ lib/generators/acronos_poc/acronos_poc_generator.rb
@@ -1,5 +1,11 @@
 require 'rails/generators/base'
 
-class AcronosPocGenerator < Rails::Generators::NamedBase
+class AcronosPocGenerator < Rails::Generators::Base
   source_root File.expand_path('../templates', __FILE__)
+  argument :controller, type: :string
+  argument :method, type: :string, default: "index"
+
+  def generate_view
+    template "#{method}.html.erb", "app/views/#{controller}/#{method}.html.erb"
+  end
 end
